44255bbc127161dfdc056c1551b40170d0a0a143
Gibran Var <104398839+gibranvar@users.noreply.github.com>
2024-12-12 16:33:13 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoUri: string = process.env.DB_URI!;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://anaygeovanni.netlify.app/"
+}));
 app.use(express.json());
 app.use('/api', guestRoutes);
 
